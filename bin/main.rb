750a70e464a643f38f8e9350511dd098ec32be57
Jair Jaramillo <jair.jaramillo@outlook.com>
2019-10-24 15:46:16 -0500

    defining structure of the program

--- bin/main.rb
+++ bin/main.rb
@@ -3,3 +3,18 @@
 # frozen_string_literal: true
 
 puts 'Hello world!'
+
+# 1.-   Welcome message
+# 2.-   Explain rules
+# 3.-   Define names of the players
+# 4.-   Select simbol ("X" or "O") X simbol is first player
+# 5.-   Clear the screen
+# 6.-   Show the empty board
+# 7.-   Asks for the player in turn to select the position
+# 8.-   Clean the screen
+# 9.-   Show the updated board
+# 10.-  Asks again until the board is filled
+# 11.-  Check for the winner
+# 12.-  Display the winner
+#
+# Every time we check for a new position, check if 1) it is a valid move and 2) there is a winner

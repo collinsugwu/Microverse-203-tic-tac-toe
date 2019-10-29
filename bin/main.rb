e7c3b4d6b38e9877d4a6f0233a0eac9b3b6da47e
Jair Jaramillo <jair.jaramillo@outlook.com>
2019-10-29 09:56:48 -0600

    adjusting the show_board method

--- bin/main.rb
+++ bin/main.rb
@@ -23,6 +23,12 @@
 #
 
 class Interface
+  attr_accessor :pos
+
+  def initialize
+    @pos = Array.new(9, ' ')
+  end
+
   def welcome
     puts 'Welcome to the Tic-Tac-Toe game'
     puts 'TODO: Explain rules'
@@ -34,22 +40,27 @@ class Interface
     Gem.win_platform? ? (system 'cls') : (system 'clear')
   end
 
+  def add(symbol, position)
+    @pos[position] = symbol
+  end
+
   def show_board
     puts 'this is the board you are playing on'
     puts 'Player 1: Collins (X)           Player 2: Jair (O)'
     puts
-    puts '1|2 |3                         X | O | X'
+    puts "1 | 2 | 3                      #{@pos[0]} | #{@pos[1]} | #{@pos[2]}"
     puts '-------                        ---------'
-    puts '4|5 |6                           | X |'
+    puts "4 | 5 | 6                      #{@pos[3]} | #{@pos[4]} | #{@pos[5]}"
     puts '-------                        ---------'
-    puts '7|8 |10                          | O | X'
+    puts "7 | 8 | 9                      #{@pos[6]} | #{@pos[7]} | #{@pos[8]}"
     puts
   end
 
-  def play_turn(player)
+  def play_turn(symbol, player)
     # check player
     puts "#{player}: select position of the symbol on the board"
-    gets.chomp
+    position = gets.chomp
+    add(symbol, position.to_i - 1)
     # save the value in a position variable
     # check if it is a valid move
     # display the symbol on the position choosen
@@ -57,7 +68,12 @@ class Interface
     # check if there is a winner or if a draw else repeat the process with the next play
   end
 
+  def error
+    puts 'invalid move, please pick another position'
+  end
+
   def display_winner
+    show_board
     puts 'Checking if there is a winner, or there is a draw...'
     # Calling the logic method to check winner
     puts 'Player 1 is the winner'
@@ -79,15 +95,15 @@ interface.welcome
 interface.clear_screen
 
 turn = 1 # needed for the mockup
-while turn < 5 # while !board.empty?
+while turn <= 9 # while !board.empty?
   interface.show_board
   if turn.odd?
-    interface.play_turn('player1') # board.add(interface.play_turn('player1'))
+    interface.play_turn('X', 'player1') # board.add(interface.play_turn('player1'))
     interface.clear_screen
     puts 'checking if the move is valid'
     puts 'adding the mark from the player 1 to the board'
   else
-    interface.play_turn('player2') # board.add(interface.play_turn('player1'))
+    interface.play_turn('O', 'player2') # board.add(interface.play_turn('player1'))
     interface.clear_screen
     puts 'checking if the move is valid'
     puts 'adding the mark from the player 2 to the board'

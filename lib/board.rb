6bfff9a205ffa193e26b1f95eb9947b142b75ed3
Jair Jaramillo <jair.jaramillo@outlook.com>
2019-10-29 12:00:28 -0600

    fixing winner method

--- lib/board.rb
+++ lib/board.rb
@@ -34,59 +34,26 @@ class Board
   # 2,5,8
   # 0,4,8
   # 2,4,6
+
   def winner
-    winner = false
+    return 'X' if @position[0] == 'X' && @position[1] == 'X' && @position[2] == 'X'
+    return 'X' if @position[3] == 'X' && @position[4] == 'X' && @position[5] == 'X'
+    return 'X' if @position[6] == 'X' && @position[7] == 'X' && @position[8] == 'X'
+    return 'X' if @position[0] == 'X' && @position[3] == 'X' && @position[6] == 'X'
+    return 'X' if @position[1] == 'X' && @position[4] == 'X' && @position[7] == 'X'
+    return 'X' if @position[2] == 'X' && @position[5] == 'X' && @position[8] == 'X'
+    return 'X' if @position[0] == 'X' && @position[4] == 'X' && @position[8] == 'X'
+    return 'X' if @position[2] == 'X' && @position[4] == 'X' && @position[6] == 'X'
 
-    3.times do |x|
-      if @position[x] == 'X'
-        winner = true
-      else
-        winner = false
-      end
-    end
+    return 'O' if @position[0] == 'O' && @position[1] == 'O' && @position[2] == 'O'
+    return 'O' if @position[3] == 'O' && @position[4] == 'O' && @position[5] == 'O'
+    return 'O' if @position[6] == 'O' && @position[7] == 'O' && @position[8] == 'O'
+    return 'O' if @position[0] == 'O' && @position[3] == 'O' && @position[6] == 'O'
+    return 'O' if @position[1] == 'O' && @position[4] == 'O' && @position[7] == 'O'
+    return 'O' if @position[2] == 'O' && @position[5] == 'O' && @position[8] == 'O'
+    return 'O' if @position[0] == 'O' && @position[4] == 'O' && @position[8] == 'O'
+    return 'O' if @position[2] == 'O' && @position[4] == 'O' && @position[6] == 'O'
 
-    3.times do |x|
-      if @position[x + 3] == 'X'
-        winner = true
-      else
-        winner = false
-      end
-    end
-
-    3.times do |x|
-      if @position[x + 6] == 'X'
-        winner = true
-      else
-        winner = false
-      end
-    end
-
-    if winner == true
-      return 'X'
-    end
-
-    return 'none'
+    'no winner'
   end
-
-  # add_symbol
-  # full?
-  # check_winner
-
-  # def empty?
-  #   @board.size.times do |i|
-  #     return false unless @board[i].zero?
-  #   end
-  #   true
-  # end
-
-  # # Board.add("X", 5)
-  # def add(position)
-  #   @turn += 1
-  #   if @turn.even?
-  #     @board[position] = 1
-  #   else
-  #     @board[position] = -1
-  #   end
-  # end
-
 end

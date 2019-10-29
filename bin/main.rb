873fd248c481da22f0f0d2fc7b93354c2cb6c9a7
Ugwu Collins <collinsuchinaka@gmail.com>
2019-10-29 22:31:03 +0100

    Board and main class refactored

--- bin/main.rb
+++ bin/main.rb
@@ -25,27 +25,19 @@ require_relative '../lib/board.rb'
 #
 
 class Interface
-  attr_accessor :pos
-
   def initialize
-    # @pos = Array.new(9, ' ')
     @board = Board.new
   end
 
   def welcome
     puts 'Welcome to the Tic-Tac-Toe game'
-    puts 'TODO: Explain rules'
+    rules
     puts 'Press ENTER key to continue'
     gets.chomp
   end
 
   def clear_screen
     Gem.win_platform? ? (system 'cls') : (system 'clear')
-  end
-
-  def add(symbol, position)
-    # @pos[position] = symbol
-    @board.position[position] = symbol
   end
 
   def display_board
@@ -60,20 +52,17 @@ class Interface
     puts
   end
 
-  def play_turn(symbol, player)
-    # check player
+  def play_turn
+    player = @board.check_turn
     puts "#{player}: select position of the symbol on the board"
     position = gets.chomp
-    add(symbol, position.to_i - 1)
-    # save the value in a position variable
-    # check if it is a valid move
-    # display the symbol on the position choosen
-    # save the move
-    # check if there is a winner or if a draw else repeat the process with the next play
-  end
-
-  def display_error
-    puts 'invalid move, please pick another position'
+    validate = @board.validate_position(position)
+    if !validate
+      puts 'Invalid Position'
+      play_turn
+    else
+      @board.position = position
+    end
   end
 
   def display_winner
@@ -83,39 +72,21 @@ class Interface
     puts 'Player 1 is the winner'
     # Display the winner of the game
   end
-end
 
-interface = Interface.new
-
-interface.clear_screen
-
-puts 'Creating Board' # board = Board.new
-puts 'Creating player 1 with "X" mark' # player1 = Player.new('X')
-puts 'Creating player 2 with "O" mark' # player2 = Player.new('O')
-puts
-
-interface.welcome
-
-interface.clear_screen
-
-turn = 1 # needed for the mockup
-while turn <= 9 # while !board.empty?
-  interface.display_board
-  if turn.odd?
-    interface.play_turn('X', 'player1') # board.add(interface.play_turn('player1'))
-    interface.clear_screen
-    puts 'checking if the move is valid'
-    puts 'adding the mark from the player 1 to the board'
-  else
-    interface.play_turn('O', 'player2') # board.add(interface.play_turn('player1'))
-    interface.clear_screen
-    puts 'checking if the move is valid'
-    puts 'adding the mark from the player 2 to the board'
+  def play
+    until @board.full?
+      display_board
+      play_turn
+    end
   end
-  puts
-  turn += 1 # simulating the turn order. The while loop should break if the board is full of if there is a winner
+
+  private
+
+  def rules
+    puts 'Obey these  rules and have a wonderfull experience'
+
+    puts 'The first person to play become player1 with a symbol of X'
+    puts 'The Second person to play become player2 with a symbol of O'
+    puts 'Each player takes a turn to select a position for the symbol'
+  end
 end
-
-puts
-
-interface.display_winner
--- lib/board.rb
+++ lib/board.rb
@@ -3,14 +3,19 @@
 require_relative 'player.rb'
 
 class Board
-  # get_board
-  attr_accessor :position, :turn
+  attr_accessor :turn
 
+  # @turn = 0
   def initialize
     @position = Array.new(9, ' ')
+    @turn = 1
   end
 
-  # empty?
+  def position=(position)
+    @position[position - 1] = @turn.odd? ? 'X' : 'O'
+    @turn += 1
+  end
+
   def empty?
     @position.size.times do |x|
       return false unless @position[x] != ' '
@@ -25,15 +30,13 @@ class Board
     true
   end
 
-  # winning combinations:
-  # 0,1,2
-  # 3,4,5
-  # 6,7,8
-  # 0,3,6
-  # 1,4,7
-  # 2,5,8
-  # 0,4,8
-  # 2,4,6
+  def check_turn
+    if @turn.odd?
+      'player 1'
+    else
+      'player 2'
+    end
+  end
 
   def winner
     3.times { |i| return 'X' if @position[0 + (i * 3)] == 'X' && @position[1 + (i * 3)] == 'X' && @position[2 + (i * 3)] == 'X' }
@@ -44,5 +47,22 @@ class Board
     2.times { |i| return 'O' if @position[0 + (i * 2)] == 'O' && @position[4] == 'O' && @position[8 + (i * 2)] == 'O' }
 
     'no winner'
+  end
+
+  def draw
+    return 'Draw' if winner == 'no winner' && full?
+  end
+
+  def validate_position(position)
+    position = position.to_i
+    return false unless position.between?(1, 9)
+    return false unless board_position(position) == ' '
+  end
+
+  private
+
+  def board_position(position)
+    position -= 1
+    @position[position]
   end
 end

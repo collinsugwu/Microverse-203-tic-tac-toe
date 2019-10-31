a77da630e75bc90ec0ee77c13741068dbda86716
Jair Jaramillo <jair.jaramillo@outlook.com>
2019-10-31 11:37:26 -0600

    refactoring and solving TSE suggestions

--- lib/board.rb
+++ lib/board.rb
@@ -4,42 +4,43 @@ class Board
   attr_reader :position, :turn
 
   def initialize
-    @position = Array.new(9, ' ')
     @turn = 1
+    @symbol_x = 'X'
+    @symbol_o = 'O'
+    @empty_space = ' '
+    @position = Array.new(9, @empty_space)
   end
 
   def position=(position)
-    @position[(position.to_i - 1)] = @turn.odd? ? 'X' : 'O'
+    @position[(position.to_i - 1)] = @turn.odd? ? @symbol_x : @symbol_o
     @turn += 1
   end
 
   def empty?
     @position.size.times do |x|
-      return false unless @position[x] != ' '
+      return false unless @position[x] != @empty_space
     end
     true
   end
 
   def full?
     @position.size.times do |x|
-      return false if @position[x] == ' '
+      return false if @position[x] == @empty_space
     end
     true
   end
 
   def check_turn
-    if @turn.odd?
-      'player 1'
-    else
-      'player 2'
-    end
+    return 1 if @turn.odd?
+
+    2
   end
 
   # returns 1 if player 1 wins, returns 2 if player 2 wins, returns 0 if there is no winner, and returns 3 if there is a draw
   def winner
     3.times do |i|
-      return 1 if all_equal(i, 'X')
-      return 2 if all_equal(i, 'O')
+      return 1 if all_equal(i, @symbol_x)
+      return 2 if all_equal(i, @symbol_o)
     end
 
     return 3 if full?
@@ -58,7 +59,7 @@ class Board
   def validate_position?(position)
     return 1 unless position.to_i.is_a? Integer
     return 2 unless position.to_i.between?(1, 9)
-    return 3 unless @position[position.to_i - 1] == ' '
+    return 3 unless @position[position.to_i - 1] == @empty_space
 
     0
   end

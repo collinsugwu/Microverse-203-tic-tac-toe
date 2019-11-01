2efd23d9d2cccb4b9311450e35149e18c6b716d5
Jair Jaramillo <jair.jaramillo@outlook.com>
2019-11-01 09:27:47 -0600

    updating board.rb

--- lib/board.rb
+++ lib/board.rb
@@ -4,46 +4,43 @@ class Board
   attr_reader :position, :turn
 
   def initialize
-    @position = Array.new(9, ' ')
     @turn = 1
+    @symbol_x = 'X'
+    @symbol_o = 'O'
+    @empty_space = ' '
+    @position = Array.new(9, @empty_space)
   end
 
   def position=(position)
-    @position[(position.to_i - 1)] = @turn.odd? ? 'X' : 'O'
+    @position[(position.to_i - 1)] = @turn.odd? ? @symbol_x : @symbol_o
     @turn += 1
   end
 
   def empty?
     @position.size.times do |x|
-      return false unless @position[x] != ' '
+      return false unless @position[x] != @empty_space
     end
     true
   end
 
   def full?
     @position.size.times do |x|
-      return false if @position[x] == ' '
+      return false if @position[x] == @empty_space
     end
     true
   end
 
   def check_turn
-    if @turn.odd?
-      'player 1'
-    else
-      'player 2'
-    end
+    return 1 if @turn.odd?
+
+    2
   end
 
   # returns 1 if player 1 wins, returns 2 if player 2 wins, returns 0 if there is no winner, and returns 3 if there is a draw
   def winner
     3.times do |i|
-      return 1 if @position[0 + (i * 3)] == 'X' && @position[1 + (i * 3)] == 'X' && @position[2 + (i * 3)] == 'X'
-      return 1 if @position[0 + i] == 'X' && @position[3 + i] == 'X' && @position[6 + i] == 'X'
-      return 2 if @position[0 + (i * 3)] == 'O' && @position[1 + (i * 3)] == 'O' && @position[2 + (i * 3)] == 'O'
-      return 2 if @position[0 + i] == 'O' && @position[3 + i] == 'O' && @position[6 + i] == 'O'
-      return 1 if @position[0 + (i * 2)] == 'X' && @position[4] == 'X' && @position[8 - (i * 2)] == 'X' && i < 2
-      return 2 if @position[0 + (i * 2)] == 'O' && @position[4] == 'O' && @position[8 - (i * 2)] == 'O' && i < 2
+      return 1 if all_equal(i, @symbol_x)
+      return 2 if all_equal(i, @symbol_o)
     end
 
     return 3 if full?
@@ -51,10 +48,18 @@ class Board
     0
   end
 
+  def all_equal(index, player_symbol)
+    return true if (@position[0 + (index * 3)] + @position[1 + (index * 3)] + @position[2 + (index * 3)] == player_symbol * 3) ||
+                   (@position[0 + index] + @position[3 + index] + @position[6 + index] == player_symbol * 3) ||
+                   (@position[0 + (index * 2)] + @position[4] + @position[8 - (index * 2)] == player_symbol * 3 && index < 2)
+
+    false
+  end
+
   def validate_position?(position)
     return 1 unless position.to_i.is_a? Integer
     return 2 unless position.to_i.between?(1, 9)
-    return 3 unless @position[position.to_i - 1] == ' '
+    return 3 unless @position[position.to_i - 1] == @empty_space
 
     0
   end
